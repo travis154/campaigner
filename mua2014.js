ab00401e1e9902b803c253e7d0de15468e3c3f99
iulogy <maldivianist@gmail.com>
2014-03-06 16:40:16 +0500

    Update mua2014.js

--- mua2014.js
+++ mua2014.js
@@ -392,7 +392,7 @@ app.post('/voters/:id/survey', authenticate, function(req,res){
 					api_secret:conf.nexmo.secret,
 					from:"MUAZ 2014",
 					to:"960" + item,
-					text: ppl.name + ", " + ppl.address + ", " + ppl.island + "votes for " + ppl.val + ".\n Status updated by " + req.user.username
+					text: ppl.name + ", " + ppl.address + ", " + ppl.island + "voting status: " + val + ".\n Status updated by " + req.user.username
 				}
 				request({
 					url:"https://rest.nexmo.com/sms/json",
